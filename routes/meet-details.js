12578efc5733724f1b2dcd7ffd50387fc255b596
Kevin Fu <kevin.fu808@gmail.com>
2015-07-17 22:13:45 -0700

    added meet details route

--- routes/meet-details.js
+++ routes/meet-details.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('sell', { title: 'Venda | Sell' });
+router.get('/:item', function(req, res, next) {
+  res.render('meet-details', { title: 'Venda | Meet Details' });
 });
 
 module.exports = router;
