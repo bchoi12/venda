0e013dbb81b49200e7633278d5a190f2f0d858e9
Kelly Yu <klyu@klyu-mn1.linkedin.biz>
2015-07-18 11:05:56 -0700

    lol stuff

--- public/javascripts/form-handler.js
+++ public/javascripts/form-handler.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
 
-  function updateTextByClass(class, newText) {
-    $(class).value = newText;
-  }
+  $('.search-dist').on('change', function() {
+    console.log(this.value);
+    console.log( $('.dist-bar .range-input').val());
+    $('.dist-bar .range-input').val($(this).val());
+  })
 
+  $('.search-cost').on('change', function() {
+    $('.cost-bar .range-input').val($(this).val());
+  })
 
+  $('.cost-bar .range-input').on('change', function() {
+    $('.search-cost').val($(this).val());
+  })
 
+  $('.dist-bar .range-input').on('change', function() {
+    $('.search-dist').val($(this).val());
+  })
 
 })
