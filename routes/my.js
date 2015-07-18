64d98e467a8ccaf20f018564d8f19bef1539ffa6
Kevin Fu <kevin.fu808@gmail.com>
2015-07-18 04:52:20 -0700

    adding my stuff

--- routes/my.js
+++ routes/my.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/items', function(req, res, next) {
-  res.render('myItems', { title: 'Venda | My Items' });
+  res.render('my-items', { title: 'Venda | My Items' });
 });
 
 /* GET home page. */
 router.get('/bids', function(req, res, next) {
-  res.render('myBids', { title: 'Venda | My Bids' });
+  res.render('my-bids', { title: 'Venda | My Bids' });
 });
 
 module.exports = router;
