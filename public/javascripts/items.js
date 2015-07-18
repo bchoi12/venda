82b82ce0cca64b4a6997e28d8750f910454ee0f2
Kevin Fu <kevin.fu808@gmail.com>
2015-07-18 03:57:31 -0700

    added bidding function js

--- public/javascripts/items.js
+++ public/javascripts/items.js
@@ -32,6 +32,32 @@ function addItem(auctionEndTime, itemName, itemType, inputPrice, minSuggestedPri
     });
   } else {
     console.log("Error! user needs to be logged in to add an item!");
+    // INSERT JQUERY HERE
   }
+
+}
+
+function setMeetingLocation(itemId, time, loc) {
+  meetupTimeRef = ref.child('items').child(itemId);
+  meetupTimeRef.update({meetupTime: time, meetupLocation: loc});
+}
+
+function bidItem(itemId, price) {
+
+  itemToBid = ref.child('items').child(itemId);
+  itemToBid.on("currentBidPrice", function(snapshot) {
+    var curPrice = snapshot.val();
+    if (curPrice >= price) {
+      console.log("Could not bid on the price. Bidding price is too low!");
+      // Insert jquery here for giving the user an error
+    } else {
+      itemToBid.update({ currentBidPrice: price });
+      console.log("Successfully bid on the price! New price is : " + price);
+      // Insert jquery telling the user that the bid went through.
+
+    }
+  }, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
+  });
 
 }
